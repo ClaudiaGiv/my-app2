5792dc89b44b687d6e6ae157fa03f8960657a565
ClaudiaGiv <clau_givan@yahoo.com>
2022-02-09 15:57:14 +0200

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,26 @@
+<script context='module'>
+
+
+	export async function load({ fetch }) {
+		console.log('Board');
+		const res = await fetch('/board1');
+		if (res.ok) {
+			const board1 = await res.json();
+
+			return {
+				props: { board1: board1 }
+			};
+		}
+		return {
+			status: res.status,
+			error: new Error()
+		};
+	}
+</script>
+<script>
+	export let board1;
+	console.log('----board----', board1);
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href='https://kit.svelte.dev'>kit.svelte.dev</a> to read the documentation</p>

c82db6685abaec55a8770dcb6e5712fcd9a65e74
ClaudiaGiv <clau_givan@yahoo.com>
2022-02-09 16:03:08 +0200

    Add netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,15 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify';
+//
+// export default {
+// 	kit: {
+// 		adapter: adapter({
+// 			// if true, will split your app into multiple functions
+// 			// instead of creating a single one for the entire app
+// 			split: false
+// 		})
+// 	}
+// };
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
